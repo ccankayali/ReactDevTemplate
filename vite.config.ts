1e81a7c17648fbbabe377bc88e9ffb6cf0a72dd3
ccankayali <volkand135@gmail.com>
2024-07-14 10:51:40 +0300

    Eslint / Prettier / Gitconfig Settings and Basic Module / Core Structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    define: {
+      'process.env': env
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      outDir: 'build'
+    }
+  }
 })
